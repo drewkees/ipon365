f05d1d768c59f8f7ab11befa2aebcc97d03d4629
Andrew C. Baldonado <acbaldonado@bounty.local>
2026-01-10 11:31:20 +0800

    reset

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -22,8 +22,8 @@ const AdBanner = () => {
       <ins
         className="adsbygoogle"
         style={{ display: "block", width: "100%", height: "90px" }}
-        data-ad-client="ca-pub-XXXXXXXXXXXXXXX" // replace with your AdSense client ID
-        data-ad-slot="1234567890"               // your Ad slot ID
+        data-ad-client="ca-pub-1805122572208587" // your real Publisher ID
+        data-ad-slot="5068460354"               // your real Ad Slot ID
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
